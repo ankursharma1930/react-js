8baf7a37a3f76a01b17bf859024bf07a7e04a1b0
ubuntu <ankur.ank1230@gmail.com>
2022-09-15 20:56:22 +0530

    upload react project

---
+++ src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <h2>Let's get started</h2>
+      <p>This is also visible now</p>
+      <h3>Hi there</h3>
+    </div>
+  );
+}
+
+export default App;
